8e1ef877c537eeb754a3a1c2af99d5d05e574eec
JohanAtWH <c.j.vanvoskuilen@windesheim.nl>
2023-12-06 17:47:46 +0100

    Updated the random walks with an interaction of 1/r^2 and a weight. Only now it equilibrates to the origin, so something is not working yet?

--- src/MonteCarloTreesApp.cpp
+++ src/MonteCarloTreesApp.cpp
@@ -12,7 +12,7 @@ using namespace ci;
 using namespace ci::app;
 using namespace std;
 
-class MonteCarloTreesApp : public App {  // NOLINT(cert-msc51-cpp)
+class MonteCarloTreesApp : public App {
   public:
 	void setup() override;
 	void update() override;
@@ -45,13 +45,16 @@ private:
 
 	// Path of the random walks
 	int numberOfSteps = 100;
-	vector<vec3> walkPath;
+	int numberOfStartingPoints = 3;
+	// Weight to define how much the interaction should add up
+	float interactionWeight = 0.1;
+	vector<vector<vec3>> walkPath;
 
 	// Step counter of the random walks
 	int currentStep = 0;
 
 	// Boolean to keep track of updating
-	bool updating;
+	bool updating = true;
 
 	// Method to generate random points in XYZ
 	static vector<vec3> generatePoints(int numPoints, float minX, float maxX, float minY, float maxY, float minZ, float maxZ);
@@ -60,6 +63,10 @@ private:
 	// Random walk is generated in spherical coordinates, with the definitions as in https://en.wikipedia.org/wiki/Spherical_coordinate_system
 	// Starting point is given with startX, startY and startZ
 	static vec3 generateRandomWalk(float startX, float startY, float startZ, float maxR = 1, float maxPhi = M_PI, float maxTheta = 2 * M_PI);
+
+	// Random walk with weight
+	// Weights increase in the direction of a starting point
+	vec3 generateWeightedRandomWalk(float x, float y, float z);
 };
 
 void MonteCarloTreesApp::setup()
@@ -72,7 +79,7 @@ void MonteCarloTreesApp::setup()
 	gl::enableDepthRead();
 
 	// Set up initial camera position
-	mCamera.lookAt(vec3(2.5, 2.5, 5), vec3(0, 0, 0), vec3(0, -1, 0));
+	mCamera.lookAt(vec3(5.0, 5.0, 5.0), vec3(0, 0, 0), vec3(0, -1, 0));
 	mCamera.setPerspective(45.0f, getWindowAspectRatio(), 0.1f, 1000.0f);
 	mCameraTarget = vec3(0);
 	mCameraUi = CameraUi(&mCamera, getWindow());
@@ -81,10 +88,10 @@ void MonteCarloTreesApp::setup()
 	currentStep = 0;
 
 	// Initialize path of the random walks
-	walkPath.resize(numberOfSteps + 1);
+	walkPath.resize(numberOfStartingPoints, vector<vec3>(numberOfSteps + 1));
 
 	// Generate initial points
-	mStartPoints = generatePoints(2, -1, 1, -1, 1, -1, 1);
+	mStartPoints = generatePoints(numberOfStartingPoints, -1, 1, -1, 1, -1, 1);
 
 	// Initialize booleans
 	updating = true;
@@ -103,22 +110,38 @@ void MonteCarloTreesApp::update()
 	if (updating) { CI_LOG_D("\tCurrentstep: \t" << currentStep); }
 
 	// Update for each step of the random walk
-	if (currentStep < walkPath.size())
+	if (currentStep < walkPath[0].size())
 	{
 		updating = true;
 		if (currentStep == 0)
 		{
-			walkPath[currentStep] = mStartPoints[0];
+			for (int index = 0; index < numberOfStartingPoints; index++)
+			{
+				walkPath[index][currentStep] = mStartPoints[index];
+			}
 		}
 		else
 		{
-			walkPath[currentStep] = generateRandomWalk(walkPath[currentStep - 1].x, walkPath[currentStep - 1].y, walkPath[currentStep - 1].z, 0.2);
+			for(int index = 0; index < numberOfStartingPoints; index++)
+			{
+				vec3 previousPoint = walkPath[index][currentStep - 1];
+				if(currentStep == 1)
+				{
+					// The first walk should not be weighted yet, since the direction of a point with a point is a Nan
+					walkPath[index][currentStep] = generateRandomWalk(previousPoint.x, previousPoint.y, previousPoint.z, 0.2);
+				}
+				else
+				{
+					walkPath[index][currentStep] = generateWeightedRandomWalk(previousPoint.x, previousPoint.y, previousPoint.z);	
+				}
+			}
+			
 		}
 
 		// Print the current position to the console
-		console() << "Step " << currentStep << ": X=" << walkPath[currentStep].x
-			<< ", Y=" << walkPath[currentStep].y
-			<< ", Z=" << walkPath[currentStep].z << endl;
+		//console() << "Step " << currentStep << ": X=" << walkPath[currentStep].x
+		//	<< ", Y=" << walkPath[currentStep].y
+		//	<< ", Z=" << walkPath[currentStep].z << endl;
 
 		std::this_thread::sleep_for(std::chrono::milliseconds(100));
 
@@ -151,11 +174,14 @@ void MonteCarloTreesApp::draw()
 
 	// Render the random walk path up to the current step
 	gl::color(walkColor);
-	gl::begin(GL_LINE_STRIP);
-	for (size_t i = 0; i < currentStep; ++i) {
-		gl::vertex(walkPath[i]);
+	for (int point = 0; point < numberOfStartingPoints; point++){
+		gl::begin(GL_LINE_STRIP);
+		for (size_t i = 0; i < currentStep; ++i)
+		{
+			gl::vertex(walkPath[point][i]);
+		}
+		gl::end();
 	}
-	gl::end();
 
 	// Display the current step number
 	gl::drawString("Step # " + std::to_string(currentStep - 1) + " / " + std::to_string(numberOfSteps), textScreenPos, textColor, Font("Arial", 12));
@@ -197,7 +223,8 @@ void MonteCarloTreesApp::keyUp(KeyEvent event) {
 vector<vec3> MonteCarloTreesApp::generatePoints(int numPoints, float minX, float maxX, float minY, float maxY, float minZ, float maxZ)
 {
 	// Seed the RNG
-	std::mt19937 rng(static_cast<unsigned int>(std::time(nullptr)));  // NOLINT(cert-msc51-cpp)
+	std::random_device rd;
+	std::mt19937 rng(rd());  // NOLINT(cert-msc51-cpp)
 
 	// Define distributions for each coordinate
 	std::uniform_real_distribution<float> xDistribution(minX, maxX);
@@ -228,7 +255,8 @@ vec3 MonteCarloTreesApp::generateRandomWalk(float startX, float startY, float st
 	}
 
 	// Seed the RNG
-	std::mt19937 rng(static_cast<unsigned int>(std::time(nullptr)));  // NOLINT(cert-msc51-cpp)
+	std::random_device rd;
+	std::mt19937 rng(rd());
 
 	// Define distributions for each coordinate
 	std::uniform_real_distribution<float> rDistribution(0, maxR);
@@ -245,4 +273,33 @@ vec3 MonteCarloTreesApp::generateRandomWalk(float startX, float startY, float st
 	return randomWalk;
 }
 
-CINDER_APP( MonteCarloTreesApp, RendererGl )
+vec3 MonteCarloTreesApp::generateWeightedRandomWalk(float x, float y, float z)
+{
+	// Generate normalized random walk
+	vec3 randomWalk = generateRandomWalk(x, y, z, 1, M_PI, 2 * M_PI);
+
+	// Loop over starting points to find attractions
+	auto inputPoint = vec3(x, y, z);
+	auto interaction = vec3 (0.0f, 0.0f, 0.0f);
+	for(auto& point : mStartPoints)
+	{
+		// Find direction between input point and starting point
+		ci::vec3 direction = glm::normalize(inputPoint - point);
+		// Find distance between input point and starting point
+		float distance = glm::length(inputPoint - point);
+		// Define interaction magnitude as 1/r^2
+		float forceMagnitude = 1.0f / (distance * distance);
+		// Calculate interaction
+		interaction += forceMagnitude * direction;
+	}
+
+	// Print the resulting interaction to the console
+	console() << "Interaction: X=" << interaction.x
+		<< ", Y=" << interaction.y
+		<< ", Z=" << interaction.z << endl;
+
+	// Calculate resulting random walk
+	return randomWalk * interaction * interactionWeight;
+}
+
+CINDER_APP(MonteCarloTreesApp, RendererGl)
