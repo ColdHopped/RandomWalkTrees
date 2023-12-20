8309f2d1d69660e9a12328d15690a0e1b6979b9c
JohanAtWH <c.j.vanvoskuilen@windesheim.nl>
2023-12-20 16:42:06 +0100

    Same.

--- src/MonteCarloTreesApp.cpp
+++ src/MonteCarloTreesApp.cpp
@@ -62,7 +62,7 @@ private:
 	// Method to generate a random walk
 	// Random walk is generated in spherical coordinates, with the definitions as in https://en.wikipedia.org/wiki/Spherical_coordinate_system
 	// Starting point is given with startX, startY and startZ
-	static vec3 generateRandomWalk(float startX, float startY, float startZ, float maxR = 1, float maxPhi = M_PI, float maxTheta = 2 * M_PI);
+	static vec3 generateRandomWalkCircle(float startX, float startY, float startZ, float maxR = 1, float maxPhi = M_PI, float maxTheta = 2 * M_PI);
 
 	// Random walk with weight
 	// Weights increase in the direction of a starting point
@@ -125,10 +125,10 @@ void MonteCarloTreesApp::update()
 			for(int index = 0; index < numberOfStartingPoints; index++)
 			{
 				vec3 previousPoint = walkPath[index][currentStep - 1];
-				if(currentStep == 1)
+				if(currentStep < 2)
 				{
 					// The first walk should not be weighted yet, since the direction of a point with a point is a Nan
-					walkPath[index][currentStep] = generateRandomWalk(previousPoint.x, previousPoint.y, previousPoint.z, 0.2);
+					walkPath[index][currentStep] = generateRandomWalkCircle(previousPoint.x, previousPoint.y, previousPoint.z, 0.1);
 				}
 				else
 				{
@@ -246,12 +246,20 @@ vector<vec3> MonteCarloTreesApp::generatePoints(int numPoints, float minX, float
 	return generatedPoints;
 }
 
-vec3 MonteCarloTreesApp::generateRandomWalk(float startX, float startY, float startZ, float maxR, float maxPhi, float maxTheta)
+vec3 MonteCarloTreesApp::generateRandomWalkCircle(float startX, float startY, float startZ, float maxR, float maxPhi, float maxTheta)
 {
 	// Input checks
 	if (maxR < 0) {
 		CI_LOG_E("Error: maxR should not be smaller than 0. Setting maxR to 0.");
 		maxR = 0;
+	}
+	if (maxPhi < 0) {
+		CI_LOG_E("Error: maxPhi should not be smaller than 0. Setting maxPhi to 0.");
+		maxPhi = 0;
+	}
+	if (maxTheta < 0) {
+		CI_LOG_E("Error: maxTheta should not be smaller than 0. Setting maxTheta to 0.");
+		maxTheta = 0;
 	}
 
 	// Seed the RNG
@@ -273,33 +281,75 @@ vec3 MonteCarloTreesApp::generateRandomWalk(float startX, float startY, float st
 	return randomWalk;
 }
 
+vec3 MonteCarloTreesApp::generateRandomWalkCartesian(float startX, float startY, float startZ, float maxX, float maxY, float maxZ)
+{
+	// Input checks
+	if (maxX < 0) {
+		CI_LOG_E("Error: maxX should not be smaller than 0. Setting maxX to 0.");
+		maxX = 0;
+	}
+	if (maxY < 0) {
+		CI_LOG_E("Error: maxY should not be smaller than 0. Setting maxY to 0.");
+		maxY = 0;
+	}
+	if (maxZ < 0) {
+		CI_LOG_E("Error: maxZ should not be smaller than 0. Setting maxZ to 0.");
+		maxZ = 0;
+	}
+
+	// Seed the RNG
+	std::random_device rd;
+	std::mt19937 rng(rd());
+
+	// Define distributions for each coordinate
+	std::uniform_real_distribution<float> rDistribution(0, maxX);
+	std::uniform_real_distribution<float> phiDistribution(0, maxY);
+	std::uniform_real_distribution<float> thetaDistribution(0, maxZ);
+
+	// Generate random walk
+	float x = rDistribution(rng);
+	float y = phiDistribution(rng);
+	float z = thetaDistribution(rng);
+
+	vec3 randomWalk = { startX + x, startY + y, startZ + z };
+
+	return randomWalk;
+}
+
 vec3 MonteCarloTreesApp::generateWeightedRandomWalk(float x, float y, float z)
 {
-	// Generate normalized random walk
-	vec3 randomWalk = generateRandomWalk(x, y, z, 1, M_PI, 2 * M_PI);
-
+	// Use current position to find interaction forces
 	// Loop over starting points to find attractions
 	auto inputPoint = vec3(x, y, z);
-	auto interaction = vec3 (0.0f, 0.0f, 0.0f);
-	for(auto& point : mStartPoints)
+	auto interaction = vec3(0.0f, 0.0f, 0.0f);
+	for (auto& point : mStartPoints)
 	{
+		// Calculate difference vector
+		vec3 difference = inputPoint - point;
 		// Find direction between input point and starting point
-		ci::vec3 direction = glm::normalize(inputPoint - point);
+		vec3 direction = glm::normalize(difference);
 		// Find distance between input point and starting point
-		float distance = glm::length(inputPoint - point);
+		float distance = glm::length(difference);
 		// Define interaction magnitude as 1/r^2
 		float forceMagnitude = 1.0f / (distance * distance);
 		// Calculate interaction
-		interaction += forceMagnitude * direction;
+		interaction -= forceMagnitude * direction;
 	}
 
+	// Take into account the interaction weight (scaling factor, to line up all units)
+	interaction = interaction * interactionWeight;
+
+	// Generate normalized random walk with interaction as magnitude
+	float r = glm::length(interaction);
+	float phi = glm::acos(interaction.z / r);
+	float theta = glm::sign(interaction.y) * glm::acos(interaction.x / glm::sqrt(pow(interaction.x, 2) + pow(interaction.y, 2)));
+	vec3 randomWalk = generateRandomWalkCircle(x, y, z, r, phi, 2 * M_PI);
+
 	// Print the resulting interaction to the console
-	console() << "Interaction: X=" << interaction.x
-		<< ", Y=" << interaction.y
-		<< ", Z=" << interaction.z << endl;
+	console() << "|Interaction| = " << glm::length(interaction) << endl;
 
 	// Calculate resulting random walk
-	return randomWalk * interaction * interactionWeight;
+	return randomWalk;
 }
 
 CINDER_APP(MonteCarloTreesApp, RendererGl)
