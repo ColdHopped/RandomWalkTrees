810609558bd9a11ff54eaeb5d5602e7b59f3b286
JohanAtWH <c.j.vanvoskuilen@windesheim.nl>
2023-12-20 16:57:00 +0100

    Added Cartesian Random Walk. Changed Interaction Random Walk to have size and direction of Weighted Interaction. Something is working; path seems to oscillate to start particle. But they still fly off.

--- src/MonteCarloTreesApp.cpp
+++ src/MonteCarloTreesApp.cpp
@@ -47,7 +47,7 @@ private:
 	int numberOfSteps = 100;
 	int numberOfStartingPoints = 5;
 	// Weight to define how much the interaction should add up
-	float interactionWeight = 0.1f;
+	float interactionWeight = 0.01f;
 	vector<vector<vec3>> walkPath;
 
 	// Step counter of the random walks
@@ -63,6 +63,11 @@ private:
 	// Random walk is generated in spherical coordinates, with the definitions as in https://en.wikipedia.org/wiki/Spherical_coordinate_system
 	// Starting point is given with startX, startY and startZ
 	static vec3 generateRandomWalkCircle(float startX, float startY, float startZ, float maxR = 1, float maxPhi = M_PI, float maxTheta = 2 * M_PI);
+
+	// Method to generate a random walk
+	// Random walk is generated in Cartesian coordinates, so cardinal directions X, Y and Z
+	// Starting point is given with startX, startY and startZ
+	static vec3 generateRandomWalkCartesian(float startX, float startY, float startZ, float maxX, float maxY, float maxZ);
 
 	// Random walk with weight
 	// Weights increase in the direction of a starting point
@@ -128,7 +133,7 @@ void MonteCarloTreesApp::update()
 				if(currentStep < 2)
 				{
 					// The first walk should not be weighted yet, since the direction of a point with a point is a Nan
-					walkPath[index][currentStep] = generateRandomWalkCircle(previousPoint.x, previousPoint.y, previousPoint.z, 0.1);
+					walkPath[index][currentStep] = generateRandomWalkCircle(previousPoint.x, previousPoint.y, previousPoint.z, 0.2);
 				}
 				else
 				{
@@ -284,32 +289,21 @@ vec3 MonteCarloTreesApp::generateRandomWalkCircle(float startX, float startY, fl
 vec3 MonteCarloTreesApp::generateRandomWalkCartesian(float startX, float startY, float startZ, float maxX, float maxY, float maxZ)
 {
 	// Input checks
-	if (maxX < 0) {
-		CI_LOG_E("Error: maxX should not be smaller than 0. Setting maxX to 0.");
-		maxX = 0;
-	}
-	if (maxY < 0) {
-		CI_LOG_E("Error: maxY should not be smaller than 0. Setting maxY to 0.");
-		maxY = 0;
-	}
-	if (maxZ < 0) {
-		CI_LOG_E("Error: maxZ should not be smaller than 0. Setting maxZ to 0.");
-		maxZ = 0;
-	}
+	// no input checks
 
 	// Seed the RNG
 	std::random_device rd;
 	std::mt19937 rng(rd());
 
 	// Define distributions for each coordinate
-	std::uniform_real_distribution<float> rDistribution(0, maxX);
-	std::uniform_real_distribution<float> phiDistribution(0, maxY);
-	std::uniform_real_distribution<float> thetaDistribution(0, maxZ);
+	std::uniform_real_distribution<float> xDistribution(maxX < 0 ? maxX : 0, maxX < 0 ? 0 : maxX);
+	std::uniform_real_distribution<float> yDistribution(maxY < 0 ? maxY : 0, maxY < 0 ? 0 : maxY);
+	std::uniform_real_distribution<float> zDistribution(maxZ < 0 ? maxZ : 0, maxZ < 0 ? 0 : maxZ);
 
 	// Generate random walk
-	float x = rDistribution(rng);
-	float y = phiDistribution(rng);
-	float z = thetaDistribution(rng);
+	float x = xDistribution(rng);
+	float y = yDistribution(rng);
+	float z = zDistribution(rng);
 
 	vec3 randomWalk = { startX + x, startY + y, startZ + z };
 
@@ -340,10 +334,10 @@ vec3 MonteCarloTreesApp::generateWeightedRandomWalk(float x, float y, float z)
 	interaction = interaction * interactionWeight;
 
 	// Generate normalized random walk with interaction as magnitude
-	float r = glm::length(interaction);
-	float phi = glm::acos(interaction.z / r);
-	float theta = glm::sign(interaction.y) * glm::acos(interaction.x / glm::sqrt(pow(interaction.x, 2) + pow(interaction.y, 2)));
-	vec3 randomWalk = generateRandomWalkCircle(x, y, z, r, phi, 2 * M_PI);
+	//float r = glm::length(interaction);
+	//float phi = glm::acos(interaction.z / r);
+	//float theta = glm::sign(interaction.y) * glm::acos(interaction.x / glm::sqrt(pow(interaction.x, 2) + pow(interaction.y, 2)));
+	vec3 randomWalk = generateRandomWalkCartesian(x, y, z, interaction.x, interaction.y, interaction.z);
 
 	// Print the resulting interaction to the console
 	console() << "|Interaction| = " << glm::length(interaction) << endl;
